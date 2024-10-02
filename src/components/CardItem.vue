3dee661875449b0e4a569b171968205bcbd99230
vgrishin <87881773+m1903164@users.noreply.github.com>
2024-10-02 19:42:11 +0300

    адаптив для карточек товара

--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -15,30 +15,54 @@ onMounted(async() => {
 </script>
 
 <template>
-  <el-card 
-    class='box-card'
-    v-for="product in product.products" :key="product.id"
-  >
-    <p class="box-card_title">
-      {{ product.title }}
-    </p>
-    <el-button 
-      type="success" 
-      plain
-      @click="showProductDetails(product)"
-    >
-      В корзину
-    </el-button>
-  </el-card>
+  <div class="card-list-container">
+    <div class="card-grid">
+      <div class="card-item" v-for="product in product.products" :key="product.id">
+        <el-card class="box-card">
+          <p class="box-card_title">{{ product.title }}</p>
+          <el-button type="success" plain @click="showProductDetails(product)">В корзину</el-button>
+        </el-card>
+      </div>
+    </div>
+  </div>
 </template>
 
 
 <style scoped>
-.box-card {
-  margin-bottom: 10px;
+.card-list-container {
+  display: flex;
+  justify-content: center;
 }
 
-.box-card_title {
-  font-size: 12px;
+.card-grid {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 20px;
+}
+
+.card-item {
+  flex-basis: 25%;
+}
+
+.box-card {
+  min-height: 200px;
+}
+
+@media (max-width: 1200px) {
+  .card-grid {
+    grid-template-columns: repeat(3, 1fr);
+  }
+}
+
+@media (max-width: 768px) {
+  .card-grid {
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+
+@media (max-width: 480px) {
+  .card-grid {
+    grid-template-columns: 1fr;
+  }
 }
 </style>
